612b27820aa94e36e8f69863f3ac0d7a1c89a930
Simone-Corso <scorso26@gmail.com>
2023-11-17 11:57:18 +0100

    fatta la cartella due

---
+++ snack-2/script.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+
+
+
+
+/*const array = [];
+
+let somma = 0;
+
+while ( somma <= 50){
+     const userNumber = parseInt(prompt('type a new number'));
+    if ( !isNaN (userNumber)){
+     somma += userNumber;
+     array.push(userNumber);
+}
+}
