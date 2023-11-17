8c1fd76ebd84e66f4e84d211c337559242838072
Simone-Corso <scorso26@gmail.com>
2023-11-17 11:48:55 +0100

    correzione al numero 50

--- snack-1/script.js
+++ snack-1/script.js
@@ -5,7 +5,7 @@ const array = [];
 
 let somma = 0;
 
-while ( somma < 50){
+while ( somma <= 50){
      const userNumber = parseInt(prompt('type a new number'));
     if ( !isNaN (userNumber)){
      somma += userNumber;
