0251116ab5a79054e4422e6ca7a12a10a51439db
Simone-Corso <scorso26@gmail.com>
2023-11-17 11:59:22 +0100

    ho scritto un passaggio in italiano

--- snack-2/script.js
+++ snack-2/script.js
@@ -1,4 +1,14 @@
+//Generatore di "nomi cognomi" casuali: il Grande Gatsby ha una lista di nomi e una lista di cognomi, e da questi due array distinti vuole generare una falsa lista di invitati con nome e cognome.
 
+
+//generare una lista nome
+
+const listaNome = [];
+
+
+//generare la lista Cognome
+
+const listaCognome = [];
 
 
 
