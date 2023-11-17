cda4cf4a1addb59c66d1a460a7e701a7c268ca07
Simone-Corso <scorso26@gmail.com>
2023-11-17 11:28:18 +0100

    realizzato un prompt per chiedere all'utente

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,1 +1,6 @@
 //Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
+
+const chiediNumeri = array [''];
+
+const richiestaUtente = prompt('Scrivi un numero qualsiasi');
+console.log(richiestaUtente);
