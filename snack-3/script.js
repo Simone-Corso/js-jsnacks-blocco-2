c06dd6e5a4f00cca3bbe39f0c53f0fc595cb6525
Simone-Corso <scorso26@gmail.com>
2023-11-20 11:38:46 +0100

    ho inserito una funzione con product con le parole

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,3 +1,8 @@
 //Dare la possibilità di inserire due parole. 
 //Verificare tramite una funzione che le due parole abbiano la stessa lunghezza. 
 //Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
+
+//inseriamo la fuction con due parole
+
+function product (parolaLunga,parolaPiccola);
+
