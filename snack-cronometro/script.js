9702b33079b12aac883c9831b2728adc497aa27c
Simone-Corso <scorso26@gmail.com>
2023-11-23 12:28:38 +0100

    problemi con realizzazione del cronometro

--- snack-cronometro/script.js
+++ snack-cronometro/script.js
@@ -1,1 +1,11 @@
 //Cliccando su un pulsante, viene avviato un cronometro. Per fermare il cronometro, bisogna cliccare su un secondo pulsante
+
+const clock = setInterval(myFunction, 10000);
+console.log(clock)
+
+function myFunction(){
+    alert('il tempo registrato è');
+}
+
+clearInterval(clock);
+
